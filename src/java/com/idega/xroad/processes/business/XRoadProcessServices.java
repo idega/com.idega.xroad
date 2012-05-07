3cf968b09951fb5ba8218440f66ea7a6cbbffabb
Valdas Žemaitis <valdas@idega.com>
2012-05-07 13:12:09 +0300

    Requiring user personal ID to be provided in order to use the methods

--- src/java/com/idega/xroad/processes/business/XRoadProcessServices.java
+++ src/java/com/idega/xroad/processes/business/XRoadProcessServices.java
@@ -17,33 +17,36 @@ public interface XRoadProcessServices {
 	/**
 	 * Fetches a list of all available processes
 	 *
+	 * @param userPersonalId - user who wishes to get BPM processes
 	 * @return  {@link Collection} of {@link XRoadProcess}
 	 */
-	public Collection<XRoadProcess> getAvailableProceses();
+	public Collection<XRoadProcess> getAvailableProceses(String userPersonalId);
 
 	/**
 	 * Fetches all cases for all processes
 	 *
+	 * @param userPersonalId - user who wishes to get cases
 	 * @return {@link Collection} of {@link CasePresentation}
 	 */
-	public Collection<CasePresentation> getAllCases();
+	public Collection<CasePresentation> getAllCases(String userPersonalId);
 
 	/**
 	 * Fetches all the cases for the process
 	 *
-	 * @param processName - name of the process (not a localized one, but "system", i.e. ApplicationForTheParkingCard)
+	 * @param userPersonalId - user who wishes to get cases
+	 * @param processName - name of the process (not a localized one, but "system", i.e. ParkingCard)
 	 * @return {@link Collection} of {@link CasePresentation}
 	 */
-	public Collection<CasePresentation> getCasesForProcess(String processName);
+	public Collection<CasePresentation> getCasesForProcess(String userPersonalId, String processName);
 
 	/**
 	 * Starts a BPM process
 	 *
-	 * @param userId - process creator
-	 * @param processName - name of the process (not a localized one, but "system", i.e. ApplicationForTheParkingCard)
+	 * @param userPersonalId - process creator
+	 * @param processName - name of the process (not a localized one, but "system", i.e. ParkingCard)
 	 * @param processData - variables for the process
 	 * @return ID of the started process
 	 */
-	public Long doSubmitProcess(String userId, String processName, Map<String, Object> processData);
+	public Long doSubmitProcess(String userPersonalId, String processName, Map<String, Object> processData);
 
 }
