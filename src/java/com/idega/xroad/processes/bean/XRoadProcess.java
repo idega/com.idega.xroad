e0b8a522591710eae426bec68a2c4263fa385c75
Rene Tomson <rene.tomson@gmail.com>
2012-05-30 14:52:33 +0300

    Revert "New fields for the bean"
    This reverts commit d741163094f7c7d0ca7eaca4266424369d22f687.

--- src/java/com/idega/xroad/processes/bean/XRoadProcess.java
+++ src/java/com/idega/xroad/processes/bean/XRoadProcess.java
@@ -2,15 +2,11 @@ package com.idega.xroad.processes.bean;
 
 import java.io.Serializable;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-
-@XmlAccessorType(XmlAccessType.FIELD)
 public class XRoadProcess implements Serializable {
 
 	private static final long serialVersionUID = -5352310685171623833L;
 
-	private String id, name, localizedName, url;
+	private String id, name, localizedName;
 
 	public String getId() {
 		return id;
@@ -34,19 +30,6 @@ public class XRoadProcess implements Serializable {
 
 	public void setLocalizedName(String localizedName) {
 		this.localizedName = localizedName;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	@Override
-	public String toString() {
-		return getName() + ", ID: " + getId() + ", localized name: " + getLocalizedName() + ", url: " + getUrl();
 	}
 
 }
