0a6f1abf32ba7e743f8d6e8d9a1f304e047d6fbb
Rene Tomson <rene.tomson@gmail.com>
2012-06-29 11:12:37 +0300

    Request SOAP-Header to Response

--- src/java/com/idega/xroad/webservices/client/OutFlowHandler.java
+++ src/java/com/idega/xroad/webservices/client/OutFlowHandler.java
@@ -1,23 +1,28 @@
 package com.idega.xroad.webservices.client;
 
-
-
+import java.util.Iterator;
 import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMNamespace;
 import org.apache.axiom.soap.SOAP11Constants;
 import org.apache.axiom.soap.SOAP12Constants;
 import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axiom.soap.SOAPFactory;
+import org.apache.axiom.soap.SOAPHeader;
 import org.apache.axiom.soap.SOAPHeaderBlock;
+import org.apache.axiom.soap.impl.llom.soap11.SOAP11HeaderBlockImpl;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.context.MessageContext;
 import org.apache.axis2.handlers.AbstractHandler;
+import org.apache.axis2.wsdl.WSDLConstants;
 
 public class OutFlowHandler extends AbstractHandler {
     public InvocationResponse invoke(MessageContext messageContext) throws AxisFault {
         // lets add an Soap header for the out going message
         SOAPEnvelope soapEnvelope = messageContext.getEnvelope();
-
+        MessageContext requestMessageContext = MessageContext.getCurrentMessageContext().getOperationContext()
+        		.getMessageContext(WSDLConstants.MESSAGE_LABEL_IN_VALUE);
+        SOAPHeader requestHeader = requestMessageContext.getEnvelope().getHeader();
+        
         if (soapEnvelope.getHeader() == null) {
             String soapNamespace = soapEnvelope.getNamespace().getNamespaceURI();
             // creating a soap factory according the the soap namespce uri
@@ -33,26 +38,12 @@ public class OutFlowHandler extends AbstractHandler {
         }
 
         OMNamespace omNamespace = OMAbstractFactory.getOMFactory().createOMNamespace("http://x-rd.net/xsd/xroad.xsd","xrd");
-        SOAPHeaderBlock consumer = soapEnvelope.getHeader().addHeaderBlock("consumer", omNamespace);
-        consumer.setText("ehub");
-        
-        SOAPHeaderBlock producer = soapEnvelope.getHeader().addHeaderBlock("producer", omNamespace);
-        producer.setText("municipality");
-        
-        SOAPHeaderBlock userId = soapEnvelope.getHeader().addHeaderBlock("userId", omNamespace);
-        userId.setText("EE12345678900");
-        
-        SOAPHeaderBlock id = soapEnvelope.getHeader().addHeaderBlock("id", omNamespace);
-        id.setText("municipality.1333026172.84603");
-        
-        SOAPHeaderBlock service = soapEnvelope.getHeader().addHeaderBlock("service", omNamespace);
-        service.setText("municipality.submitParkingCardStatement");
-        
-        SOAPHeaderBlock issue = soapEnvelope.getHeader().addHeaderBlock("issue", omNamespace);
-        issue.setText("test");
-        
-        SOAPHeaderBlock authenticator = soapEnvelope.getHeader().addHeaderBlock("authenticator", omNamespace);
-        authenticator.setText("test");
+        Iterator it = requestHeader.getChildElements();
+		while (it.hasNext()) {
+			SOAP11HeaderBlockImpl o = (SOAP11HeaderBlockImpl) it.next();
+			SOAPHeaderBlock block = soapEnvelope.getHeader().addHeaderBlock(o.getLocalName(), omNamespace);
+			block.setText(o.getText());
+		}
 
         return InvocationResponse.CONTINUE;
     }
